99f7d6a2c1a10440bd4e06fe976a0960407bc7d2
wangcongxing <2256807897@qq.com>
2021-06-19 19:48:14 -0700

    将数据库改为mysql方便和微服务对接

--- NetOpsBase/__init__.py
+++ NetOpsBase/__init__.py
@@ -1,2 +1,5 @@
+import pymysql
+pymysql.version_info = (1, 4, 0, "final", 0)
+pymysql.install_as_MySQLdb()
 from celery_tasks.celeryapp import app as celery_app
 __all__ = ('celery_app',)
--- NetOpsBase/settings.py
+++ NetOpsBase/settings.py
@@ -107,14 +107,25 @@ WSGI_APPLICATION = 'NetOpsBase.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'NAME': 'NetOpsBase',
+        'USER': 'root',
+        'PASSWORD': '123456'
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
